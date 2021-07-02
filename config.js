1e347b88b1908f33863a907b363d816d6358bda2
Gabriel Hobold Santini <gabriel.hobolds@gmail.com>
2021-07-01 22:08:22 -0300

    refractor: use youtubedl libs to download videos and playlists

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  playlistLink: 'your playlist link',
-  videoQuality: 360, // 240 or 360 or 480 or 720 or 1080
-};
--- index.js
+++ index.js
@@ -1,99 +1,99 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
+const fs = require("fs/promises");
+const { createWriteStream } = require("fs");
+const path = require("path");
+const ytdl = require("ytdl-core");
+const ytpl = require("ytpl");
+const bluebird = require("bluebird");
+const sanitizeFileName = require("sanitize-filename");
 
-const { playlistLink, videoQuality } = require('./config');
+const FILES_PATH = path.join(__dirname, "videos");
+const VIDEOS_TO_DOWNLOAD_FILE = path.join(__dirname, "videos_to_download.txt");
 
-function _delay(time) {
-  return new Promise(resolve => {
-    setTimeout(resolve, time);
-  });
-}
-
-function _isDownloadingYet(path) {
-  let dirCont = fs.readdirSync(path);
-  let files = dirCont.filter(elm => {
-    return elm.match(/.*\.(crdownload)/gi);
-  });
-  if (!files.length) {
-    return false;
-  }
-  return true;
-}
-
-function _isLastPostionOnPlaylist(playlist) {
-  const actualPostion = parseInt(playlist.split('/')[0].trim());
-  const playlistLength = parseInt(playlist.split('/')[1].trim());
-
-  if (actualPostion === playlistLength) {
-    return true;
-  }
-  return false;
-}
-
-(loop = async link => {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-
-  await page._client.send('Page.setDownloadBehavior', {
-    behavior: 'allow',
-    downloadPath: './videos',
-  });
-
-  await page.goto(link ? link : playlistLink);
-
-  await _delay(2000);
-
-  const nextButton = await page.$('.ytp-next-button');
-
-  const actualLink = await page.evaluate(() => location.href);
-
-  await page.waitForSelector(
-    'yt-formatted-string[class="index-message style-scope ytd-playlist-panel-renderer"]',
-  );
-  let elementPlaylistPosition = await page.$(
-    'yt-formatted-string[class="index-message style-scope ytd-playlist-panel-renderer"]',
-  );
-  let formattedPlaylistPosition = await elementPlaylistPosition.evaluate(
-    el => el.textContent,
-  );
-
-  await nextButton.evaluate(nextButton => {
-    nextButton.click();
-  });
-  const nextLink = await page.evaluate(() => location.href);
-  const actualLinkId = actualLink.split('&')[0].split('=')[1];
-  await page.goto(`https://www.y2mate.com/pt/youtube/${actualLinkId}`);
-
-  await page.waitForSelector(`a[data-fquality="${videoQuality}"]`);
-
-  const selectQuality = await page.$(`a[data-fquality="${videoQuality}"]`);
-
-  await selectQuality.evaluate(selectQuality => {
-    selectQuality.click();
-  });
-
-  await page.waitForSelector('.btn-file');
-
-  await _delay(1500);
-  const downloadButton = await page.$('.btn-file');
-
-  await downloadButton.evaluate(downloadButton => {
-    downloadButton.click();
-  });
-  let isDownloadingComplete = false;
-  do {
-    await _delay(2000);
-    if (!_isDownloadingYet('./videos')) {
-      isDownloadingComplete = true;
+async function createDownloadPath() {
+  try {
+    await fs.stat(FILES_PATH);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      return fs.mkdir(FILES_PATH);
     }
-  } while (!isDownloadingComplete);
 
-  console.log(`[${formattedPlaylistPosition}] 👌 downloaded: ${actualLink}`);
-
-  await browser.close();
-  if (_isLastPostionOnPlaylist(formattedPlaylistPosition)) {
-    console.log('👍 finished');
-    return;
+    throw error;
   }
-  return loop(nextLink);
+}
+
+async function getUrlsToDownload() {
+  try {
+    const file = await fs.readFile(VIDEOS_TO_DOWNLOAD_FILE, {
+      encoding: "utf8",
+    });
+
+    return file
+      .toString()
+      .split("\n")
+      .filter((url) => !!url);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      console.error(
+        `Could not find ${VIDEOS_TO_DOWNLOAD_FILE.toString()} file.`
+      );
+      process.exit(1);
+    }
+
+    throw error;
+  }
+}
+
+function isPlaylist(url) {
+  return url.includes("&list=");
+}
+
+async function extractUrlsFromPlaylist(playlistUrl) {
+  const playlistInfo = await ytpl(playlistUrl);
+  return playlistInfo.items.map((item) => item.shortUrl);
+}
+
+async function downloadVideo(url) {
+  const info = await ytdl.getInfo(url);
+  const title = info.videoDetails.title;
+
+  return new Promise((resolve, reject) => {
+    const stream = ytdl.downloadFromInfo(info, {
+      format: "18",
+    });
+
+    stream.pipe(
+      createWriteStream(
+        `${path.join(FILES_PATH, sanitizeFileName(title)).toString()}.mp4`
+      )
+    );
+
+    stream.on("error", reject);
+    stream.on("close", resolve);
+  });
+}
+
+(async () => {
+  await createDownloadPath();
+  const urls = await getUrlsToDownload();
+  const playlistUrls = urls.filter((url) => isPlaylist(url));
+  const playlistVideoUrls = await bluebird.map(
+    playlistUrls,
+    (url) => extractUrlsFromPlaylist(url),
+    {
+      concurrency: 5,
+    }
+  );
+  const videosURL = urls
+    .filter((url) => !isPlaylist(url))
+    .concat(playlistVideoUrls.flat());
+
+  await bluebird.map(
+    videosURL,
+    (url) => {
+      return downloadVideo(url);
+    },
+    {
+      concurrency: 5,
+    }
+  );
 })();
